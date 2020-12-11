22f404be49fd4ccca42f11b284f689ddee336068
ToniCaimari <acaimari@cifpfbmoll.eu>
2020-12-11 17:22:14 +0100

    p

---
+++ activitat1JavaScript_entrega/llista_paisos.js
@@ -0,0 +1,10 @@
+Pais=[]
+Continent=[]
+Llengua=[]
+Moneda=[]
+for (i=0; i<3; i++)
+{var Pais=prompt('Escriu un país: ')
+var Continent=prompt('Escriu el continent al que pertany: ')
+var Llengua=prompt('Escriu seva llengua: ')
+var Moneda=prompt('I quina moneda tenen?: ')
+document.write(`<ul><li>${Pais}<ul><li>${Continent}</li><li>${Llengua}</li><li>${Moneda}</li></ul></ul>`)};
